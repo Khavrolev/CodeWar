1019690fbc8d1899bf3318c3468d73e757f8e5e9
Khavrolev <khavrolev@gmail.com>
2021-01-29 22:10:39 +0300

    Maximum subarray sum

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string test = Kata.MakeComplement("AAAA");
+            int test = Kata.MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
             Console.WriteLine(test);
 
             Console.ReadKey();
@@ -16,15 +16,20 @@ namespace ConsoleApp1
 
         public static class Kata
         {
-            public static string MakeComplement(string dna)
+            public static int MaxSequence(int[] arr)
             {
-                char[] code = dna.ToCharArray();
-                string side = "";
-                foreach (char letter in code)
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    side += (letter == 'A') ? 'T' : (letter == 'T') ? 'A' : (letter == 'C') ? 'G' : (letter == 'G') ? 'C' : 'K';
+                    int preSum = 0;
+                    for (int j = i; j < arr.Length; j++)
+                    {
+                        preSum += arr[j];
+                        if (preSum > sum)
+                            sum = preSum;
+                    }
                 }
-                return side;
+                return sum;
             }
         }
     }
