4967eff91ec54f4a167bb5173a019d153d47b633
Khavrolev <khavrolev@gmail.com>
2021-02-02 17:09:08 +0300

    Matrix Determinant (UnDone)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int test = Kata.Score(new int[] { 4, 4, 4, 3, 3 });
+            int test = Kata.Determinant(new int[][] { new[] { 10, -10, 2, 7, -3, -6 }, new[] { -8, 5, 4, -8, 10, -7 }, new[] { 0, 6, -2, 7, 7, 5 }, new[] { -6, -10, 3, -9, 2, -2 }, new[] { 8, -4, -1, 2, 1, -10 }, new[] { -3, 4, 1, 0, 10, 6 } });
+            //int test = Kata.Determinant(new int[][] { new[] { 1 } });
             Console.WriteLine(test);
             //foreach (string t in test)
             //{
@@ -21,28 +22,34 @@ namespace ConsoleApp1
 
         public static class Kata
         {
-            public static int Score(int[] dice)
+            public static int Determinant(int[][] matrix)
             {
-                int[] score = new int[6];
-                int result = 0;
+                int det = 0;
+                int[][] cutMatrix;
+                int k;
+                List<int> result;
 
-                foreach (int side in dice)
+                if (matrix.Length == 1)
+                    det = matrix[0][0];
+                else
                 {
-                    score[side - 1]++;
-                }
-
-                for (int i = 0; i < score.Length; i++)
-                {
-                    while ((score[i] / 3) > 0)
+                    int size = matrix[0].Length;
+                    for (int i = 0; i < size; i++)
                     {
-                        result += (i + 1) * ((i == 0) ? 1000 : 100);
-                        score[i] -= 3;
+                        cutMatrix = new int[size - 1][];
+                        k = (-2 * (i % 2) + 1) * matrix[0][i];
+
+                        for (int j = 0; j < size - 1; j++)
+                        {
+                            result = matrix[j + 1].ToList();
+                            result.RemoveAt(i);
+                            cutMatrix[j] = result.ToArray();
+                        }
+                        
+                        det += k * Determinant(cutMatrix);
                     }
-
-                    result += score[i] * ((i == 0) ? 100 : (i == 4) ? 50 : 0);
                 }
-
-                return result;
+                return det;
             }
         }
     }
